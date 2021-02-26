dee3a5ebe3b5b98847755224f592ac65bfe68153
Shouvik Mandal <shouvik0013@gmail.com>
2021-02-26 21:55:48 +0530

    heroku config2

--- blog_project/settings.py
+++ blog_project/settings.py
@@ -110,8 +110,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'newstatic') # new
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'newstatic')]
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 LOGIN_REDIRECT_URL = 'home'
 LOGOUT_REDIRECT_URL = 'home'
